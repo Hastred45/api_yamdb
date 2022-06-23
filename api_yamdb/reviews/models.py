c79a852c5c2723892bd81f4edd1874962f408ab3
Gleb Kurohckin <G.Kurochking@yandex.ru>
2022-06-23 08:22:51 +0300

    test models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -25,3 +25,29 @@ class Titles(models.Model):
         on_delete=models.CASCADE,
         related_name='category'
     )
+
+
+# class Reviews(models.Model):
+#     text = models.TextField()
+#     author = models.ForeignKey(
+#         User, on_delete=models.CASCADE, related_name='posts')
+#     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    
+#     image = models.ImageField(
+#         upload_to='posts/', null=True, blank=True)
+#     group = models.ForeignKey(
+#         Group, on_delete=models.CASCADE,
+#         related_name="posts", blank=True, null=True
+#     )
+
+#     def __str__(self):
+#         return self.text
+
+# class Comment(models.Model):
+#     author = models.ForeignKey(
+#         User, on_delete=models.CASCADE, related_name='comments')
+#     post = models.ForeignKey(
+#         Post, on_delete=models.CASCADE, related_name='comments')
+#     text = models.TextField()
+#     created = models.DateTimeField(
+#         'Дата добавления', auto_now_add=True, db_index=True)
